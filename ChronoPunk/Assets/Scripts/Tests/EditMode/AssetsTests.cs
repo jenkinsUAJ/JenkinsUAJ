1d1bd95b59314039e9e5c80dbd40d14f8fbc7b23
Adelpozo04 <addelp01@ucm.es>
2026-05-14 19:47:05 +0200

    Primer diseño de test de formato de assets de arte + mover assets arte a carpeta alejada de placeholders

--- ChronoPunk/Assets/Scripts/Tests/EditMode/AssetsTests.cs
+++ ChronoPunk/Assets/Scripts/Tests/EditMode/AssetsTests.cs
@@ -1,24 +1,52 @@
-using System.Collections;
 using NUnit.Framework;
+using System.Collections;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.TestTools;
 
 public class AssetsTests
 {
-    // A Test behaves as an ordinary method
+
+    /// <summary>
+    /// Test de asset enfocado en revisar el correcto formato en los asset de arte del proyecto
+    /// </summary>
     [Test]
-    public void AssetsTestsSimplePasses()
+    public void AssetsTestsArtFormat()
     {
-        // Use the Assert class to test conditions
+        //Establecemos la carpeta raiz la cual se va a revisar
+        string rootFolder = "Assets/Art/Nuevo/CurrentArt";
+
+        //Sobre esa carpeta sacamos todos y cada uno de los assets en ella
+        string[] guids = AssetDatabase.FindAssets("", new[] { rootFolder });
+
+        foreach (string guid in guids)
+        {
+            //Sacamos el path del elemento para usarlo como referncia de nombre que debe tener
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+
+            //En caso de ser una carpeta se ignora
+            if (AssetDatabase.IsValidFolder(path))
+                continue;
+
+            string fileName = Path.GetFileNameWithoutExtension(path);
+
+            string correctFormat = System.IO.Path.ChangeExtension(path, null);
+
+            correctFormat = correctFormat.Replace("Assets/Art/Nuevo/CurrentArt/", "");
+            correctFormat = correctFormat.Replace(fileName, "");
+            correctFormat = correctFormat.Replace("/", "_");
+            correctFormat = correctFormat.ToLower();
+
+            Debug.Log($"Comprobando: {fileName} que coincida con {correctFormat} en path {path}");
+
+            Assert.IsTrue(
+                        fileName.StartsWith(correctFormat),
+                        $"{fileName} en la ruta {path} debería empezar por {correctFormat}"
+            );
+
+        }
+
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator AssetsTestsWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
-    }
 }
